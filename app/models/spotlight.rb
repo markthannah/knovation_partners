f08a4dd74eb8ad4be0f8c5e212ade2a1695d52bb
Mark Hannah <hannahm7@msn.com>
2017-01-24 12:53:55 -0500

    Got Masonry working

--- app/models/spotlight.rb
+++ app/models/spotlight.rb
@@ -1,6 +1,6 @@
 class Spotlight < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :small => "200x200", :medium => "400x400>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :small => "200x200", :medium => "400x400>", :thumb => "100x100>", :large => "600x600" }
   do_not_validate_attachment_file_type :image
 
 end
